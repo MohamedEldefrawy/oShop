355b4cb3591c6367b0bb149b3994fd45f281d78b
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2021-10-11 15:28:26 +0200

    AddingFireBaseCofig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC0WynSXOH7kXJOvXn89UN5m5bqDb4PXlI",
+    authDomain: "oshop-43969.firebaseapp.com",
+    projectId: "oshop-43969",
+    storageBucket: "oshop-43969.appspot.com",
+    messagingSenderId: "292094676143",
+    appId: "1:292094676143:web:a83de238354c90282fd85a",
+    measurementId: "G-2GCCKN1D91"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC0WynSXOH7kXJOvXn89UN5m5bqDb4PXlI",
+    authDomain: "oshop-43969.firebaseapp.com",
+    projectId: "oshop-43969",
+    storageBucket: "oshop-43969.appspot.com",
+    messagingSenderId: "292094676143",
+    appId: "1:292094676143:web:a83de238354c90282fd85a",
+    measurementId: "G-2GCCKN1D91"
+  }
 };
 
 /*
