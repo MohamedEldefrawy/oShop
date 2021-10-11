2d8250ca83064cde8ce2d0a3e4b5e0467a9ba76c
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2021-10-11 15:49:42 +0200

    CreatingNavbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
